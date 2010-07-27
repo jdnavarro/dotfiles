c09142f0c8b7179845d3987f7bf919dcfee6e03f
Danny Navarro <j@dannynavarro.net>
2010-07-27 11:22:31 +0200

    Activated shortcut to minimize windows

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -235,7 +235,7 @@ clientkeys = awful.util.table.join(
     awful.key({ modkey, "Control" }, "Return", function (c) c:swap(awful.client.getmaster()) end),
     awful.key({ modkey,           }, "o",      awful.client.movetoscreen                        ),
     awful.key({ modkey, "Shift"   }, "r",      function (c) c:redraw()                       end),
---    awful.key({ modkey,           }, "n",      function (c) c.minimized = not c.minimized    end),
+    awful.key({ modkey,           }, "n",      function (c) c.minimized = not c.minimized    end),
     awful.key({ modkey,           }, "m",
         function (c)
             c.maximized_horizontal = not c.maximized_horizontal
