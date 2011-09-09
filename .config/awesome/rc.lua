5a7758f3c7ab25464b7eb820b86c1b8724ebba06
Danny Navarro <j@dannynavarro.net>
2011-09-09 10:08:08 +0200

    screensaver settings

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -227,6 +227,7 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey, "Shift"   }, "Return", function () awful.util.spawn(browser) end),
     awful.key({ modkey, "Control" }, "r", awesome.restart),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit),
+    awful.key({ modkey, "Control" }, "l", function () awful.util.spawn("xscreensaver-command -lock") end),
 
     awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)    end),
     awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)    end),
