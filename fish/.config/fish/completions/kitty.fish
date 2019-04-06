ea442f7d8fe8a74a523ee1d167a3328a7ad62dd7
Danny Navarro <j@dannynavarro.net>
2019-04-06 13:57:04 +0100

    Kitten tweaking

---
+++ fish/.config/fish/completions/kitty.fish
@@ -0,0 +1,8 @@
+
+function __kitty_completions
+    # Send all words up to the one before the cursor
+    commandline -cop | kitty +complete fish
+end
+
+complete -f -c kitty -a "(__kitty_completions)"
+
---
+++ fish/.config/fish/functions/ssh.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function ssh --description 'alias ssh=kitty +kitten ssh'
+	kitty +kitten ssh $argv;
+end
