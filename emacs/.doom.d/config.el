2e89a781cddd5ede3b9d8c1d00fc726aa1da3e26
Danny Navarro <j@dannynavarro.net>
2020-05-03 10:44:43 +0100

    Update doom emacs settings

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -1,29 +1,57 @@
-;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-(setq doom-font (font-spec :family "Source Code Pro" :size 20)
-      doom-big-font (font-spec :family "Source Code Pro" :size 26)
-      display-line-numbers-type nil
-      org-log-done 'time
-      evil-escape-excluded-states '(normal emacs motion)
-      show-trailing-whitespace t
-      psc-ide-port 15375
-      )
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
 
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Danny Navarro"
+      user-mail-address "j@dannynavarro.net")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
 ;;
-;; Key bindings
-(map! :leader
-      (:prefix "c"
-        :desc "Sort lines alphabetically" :v "s" #'sort-lines)
-      (:prefix "f"
-        :desc "Diff init.el with init.example.el" "D"   #'ediff-private-init-and-emacs-init)
-      (:prefix "w"
-        "F" #'make-frame))
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+(setq doom-font (font-spec :family "iosevka" :size 16))
 
-(after! projectile
-   (projectile-register-project-type
-      'bazel '("WORKSPACE")
-      :compile "bazel build //..."
-      :test "bazel test //..."))
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-dracula)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/Sync/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type nil)
 
 (after! org
   (load "~/Sync/org/orgmode.el"))
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; they are implemented.
+
+(setq rustic-lsp-server 'rust-analyzer)
