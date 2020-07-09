617c12ecc8b8515b800a446141d59f0d75712928
Danny Navarro <j@dannynavarro.net>
2020-07-09 16:05:13 +0100

    Add ghcup bin dir to path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,7 +4,7 @@ if status is-login
     end
 end
 
-set -x PATH ~/.emacs.d/bin ~/.local/bin ~/.cargo/bin (yarn global bin) /usr/share/bcc/tools $PATH
+set -x PATH ~/.local/bin ~/.emacs.d/bin ~/.cargo/bin (yarn global bin) ~/.cabal/bin ~/.ghcup/bin $PATH
 set -x BROWSER "firefox"
 set -x EDITOR "nvim"
 set -x PAGER "bat"
@@ -16,3 +16,4 @@ abbr -a -- hm history merge
 abbr -a -- cf nvim ~/.config/fish/config.fish
 abbr -a -- ck nvim ~/.config/kitty/kitty.conf
 abbr -a -- ci nvim ~/.config/i3/config
+
